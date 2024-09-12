f0304f55def7a184a2619d379cdcdbceb0072166
Justin Berg <justin.berg1@verizon.com>
2024-09-12 08:18:24 -0500

    only two tests not passing, need to figure out why I cannot test my work using console.log, also had to copy and paste from my personal laptop

--- classes/Music.js
+++ classes/Music.js
@@ -1,6 +1,19 @@
-// import the Media class:
+const Media = require('./Media');
 
-// create your Music class:
+class Music extends Media {
+  constructor(title, year, genre, artist, length) {
+    super(title, year, genre);
+    this.artist = artist;
+    this.length = length;  
+  }
 
+  summary() {
+    return `Title: ${this.title}, Artist: ${this.artist}, Year: ${this.year}, Genre: ${this.genre}, Length: ${this.length} seconds`;
+  }
+
+  static shortestAlbum(musicArray) {
+    return musicArray.reduce((shortest, music) => (music.length < shortest.length ? music : shortest), musicArray[0]);
+  }
+}
 // don't change below
 module.exports = Music;
